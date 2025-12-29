f9ce5159d92ce8dfa62a596f6c842521a3f3af9d
KM <>
2025-12-29 09:33:28 +0100

    started ch15, improved compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcc -O -Wall -Wextra -Wno-unused-value -pedantic -std=c99 -o $1 $1.c
-chmod +x $1
+gcc -O -Wall -Wextra -Wno-unused-value -pedantic -std=c99 -o $1 $1.c && chmod +x $1 && ./$1
